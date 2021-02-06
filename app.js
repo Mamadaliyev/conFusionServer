9caabea2b54d91b34e49f389210a394971fc144a
shohruh1999 <shohruh@3i.ai>
2021-02-06 10:33:45 +0500

    express example with  mongoose

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const mongoose = require("mongoose");
+
+const Dishes = require("./models/dishes");
+
+const url = "mongodb://localhost:27017/conFusion";
+
+const connect = mongoose.connect(url);
+
+connect.then(
+  (db) => {
+    console.log("Connected correctly to  server");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
---
+++ models/dishes.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+require("mongoose-currency").loadType(mongoose);
+
+const Currency = mongoose.Types.Currency;
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const dishSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    price: {
+      type: Currency,
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    comments: [commentSchema],
+  },
+  { timestamp: true }
+);
+
+var Dishes = mongoose.model("Dish", dishSchema);
+
+module.exports = Dishes;
