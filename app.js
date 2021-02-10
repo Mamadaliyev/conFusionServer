9bf2c1ddf4f32bb1c044253fb5ac50ba01010514
shohruh1999 <shohruh@3i.ai>
2021-02-10 18:23:38 +0500

    added cookie management

--- app.js
+++ app.js
@@ -24,32 +24,44 @@ connect.then(
 );
 
 function auth(req, res, next) {
-  // console.log(req.headers);
+  console.log(req.signedCookies);
 
-  var authHeader = req.headers.authorization;
-  if (!authHeader) {
-    var err = new Error("You are not authenticated");
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    next(err);
-    return;
-  }
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+      var err = new Error("You are not authenticated");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      next(err);
+      return;
+    }
 
-  console.log(authHeader);
+    console.log(authHeader);
 
-  var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
-    .toString()
-    .split(":");
-  var user = auth[0];
-  var pass = auth[1];
+    var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+      .toString()
+      .split(":");
+    var user = auth[0];
+    var pass = auth[1];
 
-  if (user == "admin" && pass == "password") {
-    next(); //authorized
+    if (user == "admin" && pass == "password") {
+      res.cookie("user", "admin", { signed: true });
+      next(); //authorized
+    } else {
+      var err = new Error("You are not authenticated");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      next(err);
+    }
   } else {
-    var err = new Error("You are not authenticated");
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    next(err);
+    if (req.signedCookies.user === "admin") {
+      next();
+    } else {
+      var err = new Error("You are not authenticated");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      next(err);
+    }
   }
 }
 
@@ -62,6 +74,7 @@ var leaderRouter = require("./routes/leaderRouter");
 var app = express();
 
 // view engine setup
+app.use(cookieParser("123456-7890"));
 app.use(auth);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
